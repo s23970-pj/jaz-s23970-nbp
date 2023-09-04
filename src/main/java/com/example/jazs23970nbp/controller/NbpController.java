095dfc74f0cf8e285da330dc54578e9d4365d195
adriangoik <adekgoik58@gmail.com>
2023-09-04 16:08:57 +0200

    exception handler now is showing in swagger documentation

--- src/main/java/com/example/jazs23970nbp/controller/NbpController.java
+++ src/main/java/com/example/jazs23970nbp/controller/NbpController.java
@@ -2,12 +2,18 @@ package com.example.jazs23970nbp.controller;
 
 import com.example.jazs23970nbp.model.Currency;
 import com.example.jazs23970nbp.Service.NbpService;
+
+
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +24,20 @@ public class NbpController {
     public NbpController(NbpService nbpService) {
         this.nbpService = nbpService;
     }
+
     @Operation(summary = "Średni kurs walutowy", description = "Użytkownik dostarcza nazwe waluty wg.standard ISO 4217 format dat: YYYY-MM-DD")
-    @GetMapping("/currency/{code}/{startDate}/{endDate}")
-    public ResponseEntity<Currency> getCurrency(@ApiParam(value = "Currency code", example = "GBP") @PathVariable("code") String code,
-                                                @ApiParam(value = "Start date of range", example = "2023-06-01") @PathVariable("startDate") String startDate,
-                                                @ApiParam(value = "End date of range", example = "2023-06-12")@PathVariable("endDate") String endDate
+    @GetMapping("/currency/{code}")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode= "400", description = "Invalid query posted to NBP API"),
+            @ApiResponse(responseCode = "404", description = "NBP API haven't found request"),
+            @ApiResponse(responseCode = "500", description = "Internal Error on the server side")
+    })
+    public ResponseEntity<Currency> getCurrency(@Parameter(description = "Currency code", example = "GBP") @PathVariable("code") String code,
+                                                @Parameter(description = "From what date", example = "2023-06-01") @RequestParam("startDate") String startDate,
+                                                @Parameter(description = "Till what date", example = "2023-06-12")@RequestParam("endDate") String endDate
     ){
+        System.out.println(code+startDate+endDate);
         return ResponseEntity.ok(nbpService.getCurrency(code,startDate,endDate));
     }
 }
--- src/main/java/com/example/jazs23970nbp/exceptionhandler/GlobalControllerAdvice.java
+++ src/main/java/com/example/jazs23970nbp/exceptionhandler/GlobalControllerAdvice.java
@@ -1,22 +1,27 @@
 package com.example.jazs23970nbp.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
+    @ResponseStatus(HttpStatus.NOT_FOUND)
 
     @ExceptionHandler(HttpClientErrorException.NotFound.class)
     public ResponseEntity<Void> handleNotFound() {
         return ResponseEntity.notFound().build();
     }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(HttpClientErrorException.BadRequest.class)
     public ResponseEntity<Void> handleBadRequest() {
         return ResponseEntity.badRequest().build();
     }
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
     public ResponseEntity<Void> handleInternalServerError() {
         return ResponseEntity.internalServerError().build();

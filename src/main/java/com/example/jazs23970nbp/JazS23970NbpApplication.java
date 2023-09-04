6e5ebd12a25dc634bd93ef622aa632ab652a3553
adriangoik <adekgoik58@gmail.com>
2023-09-04 14:16:58 +0200

    Initial commit

---
+++ src/main/java/com/example/jazs23970nbp/JazS23970NbpApplication.java
@@ -0,0 +1,13 @@
+package com.example.jazs23970nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23970NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23970NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jazs23970nbp/JazS23970NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jazs23970nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23970NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
